16e699c1598c47196e21d1a4fd97b77a21e7fa98
David Kirwan <davidkirwanirl@gmail.com>
2024-08-30 08:57:54 +0100

    updated with latest versions of all dependenices
    Signed-off-by: David Kirwan <davidkirwanirl@gmail.com>

--- javascripts/resume_launcher.js
+++ javascripts/resume_launcher.js
@@ -1,20 +1,42 @@
-    var settings = {
-        showForks: false, // For Github
-        title: {
-            color: "white",
-            bold: false,
-            italic: true
-        },
-        command: {
-            color: "green",
-            bold: true,
-            italic: false,
-            backgroundColor: "pink"
-        },
-        name: {
-            color: "purple"
-        }
-    };
-    $(document).ready(function() { 
-        $("body").CMDResume("/resume/details.json", "/resume/extra-details.json", settings); 
-    });
+$(document).ready(function() {
+	var settings = {
+		showForks: false,
+		title: {
+			color: 'white',
+			bold: false,
+			italic: true
+		},
+		command: {
+			color: 'green',
+			bold: true,
+			italic: false,
+			backgroundColor: 'pink'
+		},
+		name: {
+			color: 'purple'
+		},
+		extraDetails: '/resume/extra-details.json',
+		customCommands: [
+			{
+				name: 'countries',
+				title: 'Countries',
+				description: 'countries that I\'ve been to',
+				type: 'array',
+				data: ['extra', 'countriestravelledto'],
+				handlers: {
+					organisation: function(value) {
+						return value.name;
+					},
+					title: function(value) {
+						return value.cities.join(', ');
+					},
+					date: function(value) {
+						return value.timeperiod;
+					}
+				}
+			}
+		]
+	};
+	$('body').CMDResume('/resume/details.json', settings);
+});
+
